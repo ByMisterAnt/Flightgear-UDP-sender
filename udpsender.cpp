9cea54cbf3631f3c29beb1470aa8aa0d92bce147
Ivan <62688865+ByMisterAnt@users.noreply.github.com>
2023-02-11 16:11:47 +0300

    Update udpsender.cpp

--- udpsender.cpp
+++ udpsender.cpp
@@ -4,24 +4,21 @@
 UdpSender::UdpSender(QObject *parent) : QObject(parent)
 {
     udpSocketSend = new QUdpSocket(this);
+    udpSocketSend->bind(QHostAddress::LocalHost,4900);
     timer = new QTimer(this);
     connect(timer, &QTimer::timeout, this, &UdpSender::run);
     timer->start(10);
 }
 
-void UdpSender::run()
+void UdpSender::send()
 {
-    QString str="fldslf";
     vec[0] += 0.001;
     vec[1] += 0.001;
     QByteArray datagram = QByteArray::fromRawData(
             reinterpret_cast<const char*>(vec.constData()),
             sizeof(double) * vec.size()
         );
-
-        //udpSocketSend->bind(QHostAddress(m_ip), m_port);
-    udpSocketSend->bind(QHostAddress::LocalHost,4900);
-
+    
     udpSocketSend->writeDatagram(datagram, QHostAddress::LocalHost, 4900);
 
 }

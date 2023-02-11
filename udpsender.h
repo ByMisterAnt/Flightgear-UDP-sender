bf48fb918b61baa127cb78fe254f44aa75ae04d7
Ivan <62688865+ByMisterAnt@users.noreply.github.com>
2023-02-11 16:11:51 +0300

    Update udpsender.h

--- udpsender.h
+++ udpsender.h
@@ -5,7 +5,6 @@
 #include <QByteArray>
 #include <QVector>
 #include <QAbstractSocket>
-#include <QString>
 #include <QTimer>
 
 
@@ -15,18 +14,11 @@ class UdpSender: public QObject
 
 public:
     UdpSender(QObject *parent = 0);
-
-    void run();
+    void send();
 
 private:
     QUdpSocket *udpSocketSend;
-    QUdpSocket *udpSocketGet;
-
-    QString m_ip  ;
-    quint16 m_port;
-
     QTimer *timer;
-
     QVector<double> vec = {11, 0.2, 2500, 10, 10, 10};
 };
 
